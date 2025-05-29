849294d80c2cbe9b2f12823d0b693fe34f068d6b
ssb-shree <bavachikarshree1666@gmail.com>
2025-05-29 18:30:57 +0530

    myprojects page starter

--- client/src/app/my-projects/page.jsx
+++ client/src/app/my-projects/page.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import axiosInstance from "@/services/axios";
+import React, { useEffect, useState } from "react";
 
 const myProjects = () => {
-  return <div>myProjects</div>;
+  const [isAuth, setAuth] = useState(false);
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axiosInstance.post(
+          "/api/v1/auth/check-auth",
+          {},
+          { withCredentials: true }
+        );
+        console.log(res);
+        setAuth(true);
+      } catch (error) {
+        console.log(error);
+        setAuth(false);
+      }
+    };
+    checkAuth();
+  }, []);
+  return <div>{isAuth ? "U are Authenticated" : "U are not autheticated"}</div>;
 };
 
 export default myProjects;
