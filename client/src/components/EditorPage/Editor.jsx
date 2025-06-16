2d623f13eb63c779a9c3673ee0f8f8d2d18b9069
ssb-shree <bavachikarshree1666@gmail.com>
2025-06-16 18:08:29 +0530

    add navbar and leave and end session logic

--- client/src/components/EditorPage/Editor.jsx
+++ client/src/components/EditorPage/Editor.jsx
@@ -12,6 +12,7 @@ import {
 import InputOutputBox from "./InputOutputBox";
 import toast from "react-hot-toast";
 import { toastMessage } from "@/services/toastMessage";
+import NavBar from "./NavBar";
 
 const Editor = ({ socket, authData, projectData, projectID }) => {
   const { code, lang } = projectData;
@@ -29,9 +30,11 @@ const Editor = ({ socket, authData, projectData, projectID }) => {
     <section className="bg-slate-900 text-cyan-300 w-screen h-screen">
       <ResizablePanelGroup direction="horizontal" className={`w-full h-full`}>
         <ResizablePanel defaultSize={100} className="h-[100%] w-[60%]">
-          <div className="bg-slate-800/60 border text-wrap border-slate-700 rounded-xl px-4 py-2 my-2 shadow-md text-white backdrop-blur-sm mx-2">
-            navbar and project details will go here maybe joined users too
-          </div>
+          <NavBar
+            projectID={projectID}
+            authData={authData}
+            projectData={projectData}
+          />
           <CodeEditor
             authData={authData}
             socket={socket}
