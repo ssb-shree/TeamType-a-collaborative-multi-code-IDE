6ee378de0396129cf49eb01ed8e1f7b87b36a18e
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-13 15:59:20 +0530

    ui changes in navbar

--- client/src/components/EditorPage/NavBar.jsx
+++ client/src/components/EditorPage/NavBar.jsx
@@ -3,11 +3,7 @@ import { Clipboard, ClipboardCopy, User, Users } from "lucide-react";
 import React from "react";
 import toast from "react-hot-toast";
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 import { useRouter } from "next/navigation";
 
@@ -15,18 +11,28 @@ import axiosInstance from "@/services/axios";
 
 import { useCodeStore } from "@/store/code";
 import { useAuthStore } from "@/store/user";
+import { useProjectStore } from "@/store/user";
 
 import Cookies from "js-cookie";
+import { ImExit } from "react-icons/im";
 
 const token = Cookies.get("token");
+
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  weigth: "500",
+  subsets: ["latin"],
+});
 
 const NavBar = ({ projectID, projectData, authData }) => {
   const { codeData } = useCodeStore();
   const { clientListDisplay } = useAuthStore();
   const router = useRouter();
 
-  const role =
-    authData.userID === projectData?.createdBy._id ? "owner" : "guest";
+  console.log({ projectData, authData });
+
+  const role = authData.userID === projectData?.createdBy._id ? "owner" : "guest";
 
   const endSession = async () => {
     if (role == "owner") {
@@ -55,42 +61,41 @@ const NavBar = ({ projectID, projectData, authData }) => {
   };
   return (
     <div className="bg-slate-800/60 border text-wrap border-slate-700 rounded-xl px-4 py-2 my-2 shadow-md text-white backdrop-blur-sm mx-2">
-      <div className="w-full flex justify-around items-center">
-        <button
-          className="bg-red-600 hover:bg-red-800 hover:cursor-pointer font-semibold text-sm  p-2 rounded"
-          onClick={endSession}
-        >
-          {" "}
-          {role == "owner" ? "End Session" : "Leave Session"}
-        </button>
-        <button
-          className="bg-green-600 hover:bg-green-800 hover:cursor-pointer flex justify-center gap-x-1 items-center font-semibold text-sm  p-2 rounded"
-          onClick={() => {
-            navigator.clipboard.writeText(
-              `https://team-type-a-collaborative-multi-cod.vercel.app/login?redirect_url=/editor/${projectID}`
-            );
+      <div className="w-full flex justify-between items-center">
+        <div>
+          <h1
+            className={`${spaceGrotesk.className} text-sm  md:text-xl md:text-nowrap text-blue-200`}
+          >{`${projectData.createdBy.name}'s ${projectData.name} Session`}</h1>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="hover:cursor-pointer font-semibold text-sm  p-2 rounded" onClick={endSession}>
+            <ImExit size={22} />
+          </div>
+          <div
+            className=" hover:cursor-pointer flex justify-center gap-x-1 items-center font-semibold text-sm  p-2 rounded"
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `https://team-type-a-collaborative-multi-cod.vercel.app/login?redirect_url=/editor/${projectID}`
+              );
 
-            toast.custom(toastMessage(true, "Copied Invite"));
-          }}
-        >
-          <ClipboardCopy size={18} />
-          Copy Invite
-        </button>
-        <Popover>
-          <PopoverTrigger className="bg-slate-950 flex justify-center gap-x-1 items-center font-semibold text-sm  p-2 rounded text-cyan-400">
-            <Users size={18} /> Users
-          </PopoverTrigger>
-          <PopoverContent className="bg-slate-950 text-cyan-300">
-            {clientListDisplay.map((item) => (
-              <li
-                className="border-0 border-b-2 rounded-sm border-slate-400"
-                key={item.socketID}
-              >
-                {item.userDetails.name}
-              </li>
-            ))}
-          </PopoverContent>
-        </Popover>
+              toast.custom(toastMessage(true, "Copied Invite"));
+            }}
+          >
+            <ClipboardCopy size={22} />
+          </div>
+          <Popover>
+            <PopoverTrigger className="bg-slate-950 flex justify-center gap-x-1 items-center font-semibold text-sm  p-2 rounded text-cyan-400">
+              <Users size={18} /> Users
+            </PopoverTrigger>
+            <PopoverContent className="bg-slate-950 text-cyan-300">
+              {clientListDisplay.map((item) => (
+                <li className="border-0 border-b-2 rounded-sm border-slate-400" key={item.socketID}>
+                  {item.userDetails.name}
+                </li>
+              ))}
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
     </div>
   );
